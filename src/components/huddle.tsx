e0f6c90ce1a8dc0e9a749ff0d7539ef64bde1add
Mark Nieto <nietomarkj@gmail.com>
2024-03-11 20:44:37 +0800

    2024/03/11

--- src/components/huddle.tsx
+++ src/components/huddle.tsx
@@ -22,7 +22,7 @@ const Huddle = () => {
             <h1>Build The Community Your Fans Will Love</h1>
             <p>
               Huddle re-imagines the way we build communities. You have a voice,
-              but so does your audience. Create Connection with your users as
+              but so does your audience. Create connections with your users as
               you engage in genuine discussion.
             </p>
             <a href="/" className="register">
